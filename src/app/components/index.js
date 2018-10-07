e356c06f00c74417e1336853ae772c23bdfb65d5
blashadow <blackzerogamer@gmail.com>
2018-10-07 16:46:11 -0400

    export button components

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -1,5 +1,13 @@
 import ItemResult from './itemResult/itemResultComponent'
 import QuestionComponent from './questionComponent/questionComponent'
 import Category from './category/categoryComponent'
+import { HomeButton, PlayAgainButton, ButtonImage } from './ButtonImage'
 
-export { ItemResult, QuestionComponent, Category }
+export { 
+  ItemResult, 
+  QuestionComponent, 
+  Category,
+  HomeButton,
+  PlayAgainButton,
+  ButtonImage
+}
