357263991441fecb70a4e8d2638d80f9811b1b90
blashadow <blackzerogamer@gmail.com>
2018-10-15 00:10:49 -0400

    Setup some style options for navigation bar

--- src/app/config/route.js
+++ src/app/config/route.js
@@ -2,23 +2,32 @@ import { createStackNavigator } from 'react-navigation';
 
 import { HomeScreen, QuestionScreen, ResultScreen, CategoryScreen } from '../screens'
 
+const navStyleOptions = {
+  headerStyle: {
+    backgroundColor: '#3989E1',
+  },
+  headerTintColor: '#FFF'
+}
+
 const stack = createStackNavigator({
   Home: {
     screen: HomeScreen,
     navigationOptions: {
-        header: () => null
+      header: () => null,
     }
   },
   category:{
     screen: CategoryScreen,
+    navigationOptions: navStyleOptions
   },
   questions: {
-    screen: QuestionScreen
+    screen: QuestionScreen,
+    navigationOptions: navStyleOptions
   },
   result: {
     screen: ResultScreen,
     navigationOptions: {
-        header: () => null
+      header: () => null
     }
   }
 }, {
