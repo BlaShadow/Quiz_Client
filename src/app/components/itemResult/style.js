b56fa7d6de1897574cf6dacf05e3aa02e8f2e979
blashadow <blackzerogamer@gmail.com>
2018-11-15 00:43:43 -0400

    item result font color change and reduce padding for container

--- src/app/components/itemResult/style.js
+++ src/app/components/itemResult/style.js
@@ -2,7 +2,7 @@ import { StyleSheet } from "react-native"
 
 export default StyleSheet.create({
   wrap:{
-    padding: 8,
+    padding: 2,
     flex: 1,
     flexDirection: 'row',
     alignItems: 'center'
@@ -14,6 +14,6 @@ export default StyleSheet.create({
   },
   title:{
     fontSize: 15,
-    color: 'white'
+    color: '#263238'
   }
 });
